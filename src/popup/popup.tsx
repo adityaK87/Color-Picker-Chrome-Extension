c4f0272fb0cca87a48dc978068e38273f2c760da
Aditya Kumar <adityapal1409@gmail.com>
2023-06-18 17:10:49 +0530

    getting eyeDropper and selecting the color

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,45 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useState, useCallback } from "react";
+import useEyeDropper from "use-eye-dropper";
 import "../assets/tailwind.css";
 
-const handleInput = (e) => {
-	e.preventDefault();
-	const name = e.target[0].value;
-	chrome.storage.sync.set({ name }, () => {
-		console.log(`Name is set to ${name}`);
-	});
-};
-
 const Popup = () => {
-	useEffect(() => {
-		chrome.storage.sync.get(["name"], (res) => {
-			console.log(res.name);
-		});
-	}, []);
-	return (
-		<div className='h-screen'>
-			<form
-				className=' flex justify-center item-center  py-44'
-				onSubmit={handleInput}>
-				{/* <div className='mb-4'> */}
-				<input
-					className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-					id='username'
-					name='name'
-					type='text'
-					placeholder='Username'
-				/>
-				{/* </div> */}
+	const { open, close, isSupported } = useEyeDropper();
+	const [color, setColor] = useState("#fff");
+	const [colorValue, setColorValue] = useState(null);
+	const [error, setError] = useState();
 
-				{/* <div className='flex items-center justify-between'> */}
+	const gettingCurrentTab = async () => {
+		let [tab] = await chrome.tabs.query({
+			active: true,
+			currentWindow: true,
+		});
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: pickColor,
+		});
+	};
+
+	// useEyeDropper will reject/cleanup the open() promise on unmount,
+	// so setState never fires when the component is unmounted.
+	const pickColor = useCallback(() => {
+		// Using async/await (can be used as a promise as-well)
+		const openPicker = async () => {
+			try {
+				const color = await open();
+				console.log(color);
+				setColor(color.sRGBHex);
+				setColorValue(color.sRGBHex);
+			} catch (e) {
+				console.log(e);
+				// Ensures component is still mounted
+				// before calling setState
+				if (!e.canceled) setError(e);
+			}
+		};
+		openPicker();
+	}, [open]);
+	return (
+		<div className='h-screen flex flex-col justify-center items-center text-center'>
+			<p className='text-lg font-medium text-center'>
+				Welcome to my color picker
+			</p>
+			{isSupported() ? (
 				<button
-					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-					type='submit'>
-					Submit
+					className='text-lg bg-yellow-300 w-40'
+					onClick={pickColor}>
+					Pick Color
 				</button>
-				{/* </div> */}
-			</form>
+			) : (
+				<span>EyeDropper API not supported in this browser</span>
+			)}
+			<div className='flex flex-row m-4 '>
+				<span
+					className='w-10 h-10 inline-block'
+					style={{ background: color }}></span>
+				<span className='text-lg'>{colorValue}</span>
+			</div>
+			{!!error && <span>{error}</span>}
 		</div>
 	);
 };
-
 export default Popup;
