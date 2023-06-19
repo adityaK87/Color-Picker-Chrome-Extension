e22cac1c415bf388122b6ca5810ac16536f78bae
Aditya Kumar <adityapal1409@gmail.com>
2023-06-19 21:33:25 +0530

    add icons

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from "react";
 import useEyeDropper from "use-eye-dropper";
 import "../assets/tailwind.css";
+import { BiCopy } from "react-icons/bi";
 
 const Popup = () => {
 	const { open, close, isSupported } = useEyeDropper();
@@ -27,6 +28,9 @@ const Popup = () => {
 			try {
 				const color = await open();
 				console.log(color);
+				chrome.storage.local.set({ color: color.sRGBHex }, () => {
+					console.log("color has been stored in the storage");
+				});
 				setColor(color.sRGBHex);
 				setColorValue(color.sRGBHex);
 			} catch (e) {
@@ -38,6 +42,10 @@ const Popup = () => {
 		};
 		openPicker();
 	}, [open]);
+
+	function handleCopyColor() {
+		navigator.clipboard.writeText(colorValue);
+	}
 	return (
 		<div className='h-screen flex flex-col justify-center items-center text-center'>
 			<p className='text-lg font-medium text-center'>
@@ -45,7 +53,7 @@ const Popup = () => {
 			</p>
 			{isSupported() ? (
 				<button
-					className='text-lg bg-yellow-300 w-40'
+					className='text-lg bg-yellow-300 w-40 rounded-lg'
 					onClick={pickColor}>
 					Pick Color
 				</button>
@@ -54,13 +62,19 @@ const Popup = () => {
 			)}
 			<div className='flex flex-row m-4 '>
 				{colorValue === null || undefined ? (
-					<span>Please select a Color</span>
+					<span className='text-base '>Please select a Color.</span>
 				) : (
 					<>
 						<span
 							className='w-10 h-10 inline-block'
 							style={{ background: color }}></span>
-						<span className='text-lg'>{colorValue}</span>
+						<span className='text-lg mx-2'>{colorValue}</span>
+						<button
+							type='submit'
+							className=' bg-slate-200 w-5 h-5 mx-12 border-2 border-solid divide-black-400'
+							onClick={handleCopyColor}>
+							<BiCopy />
+						</button>
 					</>
 				)}
 			</div>
